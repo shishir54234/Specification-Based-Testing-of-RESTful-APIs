fb5d59b715d04e97043be3a304f83d8702e7be08
shishir54234 <shishir.shahi@iiitb.ac.in>
2025-03-21 14:33:42 +0530

    changed casestudy.txt

--- test_libapplication.cpp
+++ test_libapplication.cpp
@@ -7,15 +7,22 @@
 using namespace std;
 
 void test_everything(){
-    std::vector<std::unique_ptr<Decl>> globals;
-    globals.push_back(std::make_unique<Decl>("U", std::make_unique<MapType>(std::make_unique<TypeConst>("string"), std::make_unique<TypeConst>("string"))));
-    globals.push_back(std::make_unique<Decl>("S", std::make_unique<MapType>(std::make_unique<TypeConst>("string"), std::make_unique<RecordType>(std::vector<std::pair<std::string, std::unique_ptr<TypeExpr>>>{
-        {"name", std::make_unique<TypeConst>("string")},
-        {"password", std::make_unique<TypeConst>("string")}
-    }))));
 
-    vector<unique_ptr<API>> apis;
+    // register admin register student
+    std::vector<std::unique_ptr<Decl>> globals;
+    // U : (string -> string) map // Users
+    globals.push_back(std::make_unique<Decl>("U", std::make_unique<MapType>(std::make_unique<TypeConst>("string"), std::make_unique<TypeConst>("string"))));
+    // S : (string -> {string, string}) map // Students
+    globals.push_back(std::make_unique<Decl>("S", std::make_unique<MapType>(std::make_unique<TypeConst>("string"), std::make_unique<TupleType>(std::make_unique<TypeConst>("string"), std::make_unique<TypeConst>("string")))));
+    // T : (string -> string)
+    globals.push_back(std::make_unique<Decl>("T", std::make_unique<MapType>(std::make_unique<TypeConst>("string"), std::make_unique<TypeConst>("string"))));
+    // B:(string->Book) How do we write this? Maybe we need custom types? Or do we simplify the Book into a tuple type ?
+    // R : Request Set // Requests
+    //BS:BookStudent Set // Book-Student Records
+        vector<unique_ptr<API>>
+            apis;
     {
+        // register_admin
         vector<unique_ptr<Expr>> pre_args;
         pre_args.push_back(std::make_unique<Var>("username"));
         vector<unique_ptr<Expr>> h;
@@ -42,6 +49,8 @@ void test_everything(){
         apis.push_back(std::move(api));
     }
     {
+        // register_student_ok
+        // not in (email , S)
         vector<unique_ptr<Expr>> pre_args;
         pre_args.push_back(std::make_unique<Var>("email"));
         vector<unique_ptr<Expr>> h;
@@ -73,36 +82,38 @@ void test_everything(){
         apis.push_back(std::move(api));
     }
     {
+        // authenticate user ---> rewrote this (login_ok)
+        // PRECONDITION: U[username] = password
         vector<unique_ptr<Expr>> pre_args;
-        vector<unique_ptr<Expr>> t_args;
-        t_args.push_back(std::make_unique<Var>("t"));
-        auto token_check = std::make_unique<FuncCall>("->", move(t_args));
-        vector<unique_ptr<Expr>> u_args;
-        u_args.push_back(std::make_unique<Var>("U"));
-        auto user_check = std::make_unique<FuncCall>("in", move(u_args));
-        vector<unique_ptr<Expr>> role_check_args;
-        role_check_args.push_back(std::make_unique<Var>("ADMIN"));
-        role_check_args.push_back(std::make_unique<FuncCall>("role", {}));
-        auto role_check = std::make_unique<FuncCall>("in", move(role_check_args));
-        vector<unique_ptr<Expr>> id_check_args;
-        id_check_args.push_back(std::make_unique<Var>("studentId"));
-        id_check_args.push_back(std::make_unique<FuncCall>("id", {}));
-        auto id_check = std::make_unique<FuncCall>("=", move(id_check_args));
-        pre_args.push_back(std::make_unique<FuncCall>("AND", move(pre_args)));
-        auto pre = std::make_unique<FuncCall>("AND", move(pre_args));
-        
-        vector<unique_ptr<Expr>> call_args;
-        call_args.push_back(std::make_unique<Var>("t"));
-        call_args.push_back(std::make_unique<Var>("studentId"));
-        auto call = std::make_unique<APIcall>(make_unique<FuncCall>("getStudent", move(call_args)), Response(HTTPResponseCode::OK_200, nullptr));
-        
+        vector<unique_ptr<Expr>> pre1;
+        pre1.push_back(std::make_unique<Var>("U"));
+        pre1.push_back(std::make_unique<Var>("username"));
+        pre_args.push_back(std::make_unique<FuncCall>("[]",move(pre1)));
+        pre_args.push_back(std::make_unique<Var>("password"));  
+        auto pre = std::make_unique<FuncCall>("=", move(pre_args));
+
+        // call : authenticate(username : string, password : string) ==> (OK, token)
+        vector<unique_ptr<Expr>> call_args1;
+        call_args1.push_back(std::make_unique<Var>("username"));
+        call_args1.push_back(std::make_unique<Var>("password"));
+        auto call = std::make_unique<APIcall>(make_unique<FuncCall>("register_student", move(call_args1)), Response(HTTPResponseCode::OK_200, nullptr));
+
+        // POSTCONDITION: T' = T[token -> username]
         vector<unique_ptr<Expr>> post_args;
-        post_args.push_back(std::make_unique<Var>("studentId"));
-        post_args.push_back(std::make_unique<FuncCall>("id", {}));
-        auto post = make_unique<FuncCall>("=", move(post_args));
-        
-        auto api = std::make_unique<API>(std::move(pre), std::move(call), Response(HTTPResponseCode::OK_200, std::move(post)));
-        apis.push_back(std::move(api));
+        vector<unique_ptr<Expr>> pos1;
+        pos1.push_back(make_unique<Var>("T"));
+        post_args.push_back(make_unique<FuncCall>("'",move(pos1)));
+        vector<unique_ptr<Expr>> pos2;
+        vector<unique_ptr<Expr>> pos21;
+        pos21.push_back(make_unique<Var>("token"));
+        pos21.push_back(make_unique<Var>("username"));
+        pos2.push_back(make_unique<Var>("T"));
+        pos2.push_back(make_unique<Tuple>(move(pos21)));
+        post_args.push_back(make_unique<FuncCall>("union", move(pos2)));
+
+
+        // full thing:
+        auto api = std::make_unique<API>(std::move(pre), std::move(call), Response(HTTPResponseCode::OK_200, make_unique<FuncCall>("=", move(post_args))));
     }
 
 }
